e142bf087618a88dcd3789107e635ac804d70a9a
vishalthakurr <vishalthakurr302@gmail.com>
2021-10-06 11:45:59 +0530

    stage 7  router_react or context start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-// import logo from './logo.svg';
+
 import './App.css';
-import Navbar from './modules/Navbar';
+import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
+
 
 function App() {
   return (
-    <Navbar />
+    <>
+
+      <Router>
+
+        <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+
+          </Switch>
+
+        </div>
+      </Router>
+
+
+    </>
   );
 }
 
